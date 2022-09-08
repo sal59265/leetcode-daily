178fb83bc98b04a912ec07e3e05af2ccd2264a9b
sal59265 <lee.soul12@gmail.com>
2022-09-08 14:45:46 -0400

    finish reverse string

---
+++ resverse-string.js
@@ -0,0 +1,11 @@
+let reverseString = function(s) {
+    let start = 0;
+    let end = s.length - 1;
+    while(start < end) {
+        let temp = s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        start++;
+        end--;
+    }
+};
