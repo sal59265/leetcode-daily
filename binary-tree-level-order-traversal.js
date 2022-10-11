bcd1951362205b61bf1c932500db0448b0fe0848
Soul Lee <lee.soul12@gmail.com>
2022-10-10 23:04:41 -0400

    finish binary tree level order traversal

---
+++ binary-tree-level-order-traversal.js
@@ -0,0 +1,18 @@
+var levelOrder = function (root) {
+  if (!root) return [];
+
+  const queue = [root];
+  const result = [];
+
+  while (queue.length) {
+    let len = queue.length;
+    result.push(queue.map((node) => node.val));
+    while (len--) {
+      let node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+
+  return result;
+};
